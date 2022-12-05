b0b7a8e369a75035e079ca1ad663eb68b1e03f3b
Mike <miketandy@protonmail.com>
2022-12-05 16:50:47 +0100

    connect client to api and fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import styles from "./App.module.css";
+import { Job, useJobs } from "./hooks/useJobs";
 
 function App() {
-  return <div className={styles.app}>app</div>;
+  const jobs = useJobs();
+
+  console.log(jobs);
+
+  return (
+    <div className={styles.app}>
+      {jobs ? (
+        jobs.map((job: Job) => {
+          return (
+            <div key={job.ID}>
+              <p>{job.title}</p>
+              <p>{job.organisation}</p>
+              <p>{job.description}</p>
+              <p>{job.priority}</p>
+              <p>{job.status}</p>
+            </div>
+          );
+        })
+      ) : (
+        <>loading...</>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/useJobs.ts
+++ src/hooks/useJobs.ts
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
+export interface Job {
+  CreatedAt: string;
+  DeletedAt: string;
+  ID: number;
+  UpdatedAt: string;
+  description: string;
+  organisation: string;
+  priority: 8;
+  status: string;
+  title: string;
+}
+
 export const useJobs = () => {
-  // fetch jobs
-  // save jobs in state
-  // return the state
+  const [jobs, setJobs] = useState<Job[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3002/jobs")
+      .then((response) => response.json())
+      .then((data) => setJobs(data));
+  }, []);
+
+  return jobs;
 };
