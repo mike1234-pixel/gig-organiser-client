b4f1b82ebaa5c3d07bde37dd86218d04b73610b8
Mike <miketandy@protonmail.com>
2023-01-02 17:18:19 +0100

    fix validation schema labels

--- src/components/features/Forms/AddAction/validationSchema.ts
+++ src/components/features/Forms/AddAction/validationSchema.ts
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object({
-  name: yup.string().min(2).required(),
-  description: yup.string().min(2).required(),
-  complete_by: yup.string().required(),
-  jobid: yup.number().required(),
+  name: yup.string().min(2).required().label("Name"),
+  description: yup.string().min(2).required().label("Description"),
+  complete_by: yup.string().required().label("Completed By"),
+  jobid: yup.number().required().label("Job"),
 });
--- src/components/features/Forms/AddJob/validationSchema.ts
+++ src/components/features/Forms/AddJob/validationSchema.ts
@@ -1,9 +1,9 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object({
-  description: yup.string().min(2).required(),
-  organisation: yup.string().min(2).required(),
-  priority: yup.number().required(),
-  status: yup.string().min(2).required(),
-  title: yup.string().min(2).required(),
+  description: yup.string().min(2).required().label("Description"),
+  organisation: yup.string().min(2).required().label("Organisation"),
+  priority: yup.number().required().label("Priority"),
+  status: yup.string().min(2).required().label("Status"),
+  title: yup.string().min(2).required().label("Title"),
 });
